4453a27f1ce7eb9027846e049d8c9db59ea27764
Hannah Fligel <fligelhannah@gmail.com>
2021-08-28 12:17:59 -0500

    comments added through first part of the stretch goal

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,59 +1,57 @@
-// console.log('***** Music Collection *****')
-// - Create a variable `collection` that starts as an empty array.
+// Create a variable `collection` that starts as an empty array.
 let collection = [];
-// - Add a function named `addToCollection`. This function should: 
+// Add a function named `addToCollection`. This function should: 
+// Take in the album's `title`, `artist`, `yearPublished` as input parameters
 function addToCollection(title, artist, yearPublished){
-//   - Take in the album's `title`, `artist`, `yearPublished` as input parameters
-//   - Create a new object having the above properties
-let album = { 
-    // TODO - add properties here
-    title: title,
-    artist: artist,
-    yearPublished: yearPublished
-  }
-//   - Add the new object to the end of the `collection` array
-collection.push(album);
-//   - Return the newly created object
-return album;
+    // Create a new object having the above properties
+    let album = { 
+        // TODO - add properties here
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    //  Add the new object to the end of the `collection` array
+    collection.push(album);
+    //  Return the newly created object
+    return album;
 }// fucntion end 
-// - Test the `addToCollection` function:
-//   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
-//   - Console.log each album as added using the returned value.
+
+// Test the `addToCollection` function:
+// Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+//  Console.log each album as added using the returned value.
 console.log(addToCollection('Happier Than Ever', 'Billie Eilish', 2021));
 console.log(addToCollection('When We All Fall Asleep, Where Do We Go?', 'Billie Eilish', 2019));
 console.log(addToCollection('Apricot Princess', 'Rex Orange County', 2017));
 console.log(addToCollection('Abbey Road', 'The Beatles', 1969));
 console.log(addToCollection('Back to Black', 'Amy Winehouse', 2006));
 console.log(addToCollection('OK Computer', 'Radiohead', 1997));
-//   - After all are added, console.log the `collection` array.
+//  After all are added, console.log the `collection` array.
 console.log(collection);
 
-// - Add a function named `showCollection`. This function should:
-//   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+// Add a function named `showCollection`. This function should:
+// Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
 function showCollection(array){
-//   - Console.log the number of items in the array.
+//  Console.log the number of items in the array.
     console.log("Number of albums:", array.length); 
-    //   - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
+    //Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
     for(let i=0; i<array.length; i++){
     console.log(array[i].title,"by", array[i].artist, "published in", array[i].yearPublished);
     }
 }
-// - Test the `showCollection` function.
+// Test the `showCollection` function.
 showCollection(collection);
 
-
-
-// - Add a function named `findByArtist`. This function should:
-//   - Take in `artist` (a string) parameter
+//Add a function named `findByArtist`. This function should:
+//Take in `artist` (a string) parameter
 function findByArtist(artist){
-//   - Create an array to hold any results, empty to start
+// Create an array to hold any results, empty to start
     let matches=[]; 
-    //   - Loop through the `collection` and add any objects with a matching artist to the array.
+    // Loop through the `collection` and add any objects with a matching artist to the array.
     for (let i=0; i<collection.length; i++){
         if( artist === collection[i].artist ){
             matches.push( collection[i] );
         } // end match
-    //   - Return the array with the matching results. If no results are found, return an empty array.
+    // Return the array with the matching results. If no results are found, return an empty array.
     }//end function
     return matches;
 }
@@ -66,28 +64,27 @@ console.log(findByArtist('Lizzo'));
 
 
 
-// Create a function called `search`. This function should:
-//   - Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
-//   - The returned output from `search` should meet these requirements: 
-//     - Return a new array of all items in the `collection` matching *all* of the search criteria.
-//     - If no results are found, return an empty array.
-//     - If there is no search object or an empty search object provided as input, then return all albums in the `collection`.
 
-//c
+
+// Create a function called `search` and the argument of 'input'. 
 function search(input){
-    // (yay for the internet!)
+    // if input equals "undefined" (no value is assigned to input), or if an empty object was passed (yay for the internet!), return all albums in the collection.
     if(input === undefined || Object.keys(input).length === 0 ){
     return console.log(collection);
     }//end if 
 
+//create an empty array and call it 'matches' in order to store all the user entered matches.
     let matches = [];
+    //create a for loop to loop through the collection and push each match into the new array 
     for (let i=0; i<collection.length; i++){
         if( input.artist === collection[i].artist && input.year === collection[i].yearPublished ){
             matches.push( collection[i] );
         }
     }
+    //return matches
     return console.log("Matches in Collection:", matches);
 }//end function
+
 search();
 search({ artist: 'Billie Eilish', year: 2021 });
 search({ artist: 'Finneas', year: 2020 });
